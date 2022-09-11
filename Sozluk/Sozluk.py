bc19b95605ec70fa6a7cc8c10325c8ecc5768791
Yusuf <Yusuf@YZG-LENOVO>
2022-09-11 16:16:28 +0300

    Add project files.

---
+++ Sozluk/Sozluk.py
@@ -0,0 +1,17 @@
+﻿harfler = input("Harfleri girin?")
+print(list (harfler))
+
+kelimeler = ['AB','AÇ','AD','AF','AĞ','AH','AK','AL','AM','AN','AR','AS','AŞ','AT','AV','AY','AZ']
+
+for i in range(len(kelimeler)):
+    kelimeler[i] = kelimeler[i].lower()
+
+for kelime in kelimeler:
+        hepsi_var = all([harf in harfler for harf in kelime])
+        if hepsi_var == True:
+            print(kelime)
+          
+        else:
+            continue
+
+
